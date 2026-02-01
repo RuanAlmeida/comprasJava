8b1eedc0875f9b52a25378bfd8ca68cc79c36b51
Ruan Almeida <ruan.aalmeidasantos@gmail.com>
2026-02-01 20:27:58 -0300

    first commit

---
+++ src/Compra.java
@@ -0,0 +1,14 @@
+public class Cartao {
+
+    private int limite;
+    private String bandeira;
+
+
+    public int getLimite() {
+        return limite;
+    }
+
+    public String getBandeira() {
+        return bandeira;
+    }
+}
